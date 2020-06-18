511da2c12ff188e806912f47c9947d181af413b8
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-06-18 23:44:36 +0300

    tables

--- migration.js
+++ migration.js
@@ -10,3 +10,33 @@ db.run(`
     is_current_employee INTEGER DEFAULT 1
   )
 `)
+
+db.run(`
+  CREATE TABLE Timesheet (
+    id INTEGER PRIMARY KEY,
+    hours INTEGER NOT NULL,
+    rate INTEGER NOT NULL,
+    date INTEGER NOT NULL,
+    employee_id INTEGER NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES Employee (id)
+  )
+`)
+
+db.run(`
+  CREATE TABLE Menu (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL
+  )
+`)
+
+db.run(`
+  CREATE TABLE MenuItem (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    inventory INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    menu_id INTEGER NOT NULL,
+    FOREIGN KEY (menu_id) REFERENCES Menu (id)
+  )
+`)
