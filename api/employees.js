a5afa3467dfcf39664190bb59a376c84e8f73b02
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-06-21 21:37:17 +0300

    employee api delete, put

--- api/employees.js
+++ api/employees.js
@@ -4,6 +4,17 @@ const employeesRouter = express.Router()
 const sqlite = require('sqlite3')
 const db = new sqlite.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+const validEmployee = employee => {
+  const valid = employee.name && employee.position && employee.wage
+  if (!valid) {
+    return false
+  }
+  if (employee.isCurrentEmployee === undefined) {
+    employee.isCurrentEmployee = 1
+  }
+  return true
+}
+
 employeesRouter.get('/', (req, res, next) => {
   const sql = 'SELECT * FROM Employee WHERE is_current_employee = 1'
   db.all(sql, (error, rows) => {
@@ -16,20 +27,16 @@ employeesRouter.get('/', (req, res, next) => {
 
 employeesRouter.post('/', (req, res, next) => {
   const employee = req.body.employee
-  const valid = employee.name && employee.position && employee.wage
-  if (!valid) {
+  if (!validEmployee(employee)) {
     return res.sendStatus(400)
   }
-  if (!employee.isCurrentlyEmployee) {
-    employee.isCurrentlyEmployee = 1
-  }
   const sql = 'INSERT INTO Employee (name, position, wage, is_current_employee)' +
-    'VALUES ($name, $position, $wage, $isCurrentlyEmployee)'
+    'VALUES ($name, $position, $wage, $isCurrentEmployee)'
   const data = {
     $name: employee.name,
     $position: employee.position,
     $wage: employee.wage,
-    $isCurrentlyEmployee: employee.isCurrentlyEmployee
+    $isCurrentEmployee: employee.isCurrentEmployee
   }
   db.run(sql, data, function (error) {
     if (error) {
@@ -63,6 +70,54 @@ employeesRouter.param('employeeId', (req, res, next, employeeId) => {
 
 employeesRouter.get('/:employeeId', (req, res, next) => {
   res.status(200).json({ employee: req.employee })
+})
+
+employeesRouter.put('/:employeeId', (req, res, next) => {
+  const employee = req.body.employee
+  if (!validEmployee(employee)) {
+    return res.sendStatus(400)
+  }
+  const sql = 'UPDATE Employee SET name = $name, position = $position,' +
+    'wage = $wage, is_current_employee = $isCurrentEmployee WHERE id = $id'
+  const params = {
+    $id: req.employee.id,
+    $name: employee.name,
+    $position: employee.position,
+    $wage: employee.wage,
+    $isCurrentEmployee: employee.isCurrentEmployee
+  }
+  db.run(sql, params, (error) => {
+    if (error) {
+      console.log('here')
+      return next(error)
+    }
+    const sql = 'SELECT * FROM Employee WHERE id = $id'
+    const params = { $id: req.employee.id }
+    db.get(sql, params, (error, row) => {
+      if (error) {
+        return next(error)
+      }
+      res.status(200).json({ employee: row })
+    })
+  })
+})
+
+employeesRouter.delete('/:employeeId', (req, res, next) => {
+  const sql = 'UPDATE Employee SET is_current_employee = 0 WHERE id = $id'
+  const params = { $id: req.employee.id }
+  db.run(sql, params, (error) => {
+    if (error) {
+      return next(error)
+    }
+    const sql = 'SELECT * FROM Employee WHERE id = $id'
+    const params = { $id: req.employee.id }
+    db.get(sql, params, (error, row) => {
+      if (error) {
+        return next(error)
+      }
+      res.status(200).json({ employee: row })
+    })
+  })
 })
 
 module.exports = employeesRouter
