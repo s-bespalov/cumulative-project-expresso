bb3cdbcaf306faea4100cac2f6aba835e906518e
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-06-24 00:23:43 +0300

    menu api post

--- api/menus.js
+++ api/menus.js
@@ -41,4 +41,68 @@ menusRouter.post('/', (req, res, next) => {
   })
 })
 
+menusRouter.param('menuId', (req, res, next, menuId) => {
+  const sql = 'SELECT * FROM Menu WHERE id = $id'
+  const params = { $id: menuId }
+  db.get(sql, params, (error, row) => {
+    if (error) {
+      return next(error)
+    }
+    if (!row) {
+      return res.sendStatus(404)
+    }
+    req.menu = row
+    next()
+  })
+})
+
+menusRouter.get('/:menuId', (req, res, next) => {
+  res.status(200).json({ menu: req.menu })
+})
+
+menusRouter.put('/:menuId', (req, res, next) => {
+  const menu = req.body.menu
+  if (!isValidMenu(menu)) {
+    return res.sendStatus(400)
+  }
+  const sql = 'UPDATE Menu SET title = $title WHERE id = $id'
+  const params = {
+    $id: req.menu.id,
+    $title: menu.title
+  }
+  db.run(sql, params, (error) => {
+    if (error) {
+      return next(error)
+    }
+    const sql = 'SELECT * FROM Menu WHERE id = $id'
+    const params = { $id: req.menu.id }
+    db.get(sql, params, (error, row) => {
+      if (error) {
+        return next(error)
+      }
+      res.status(200).json({ menu: row })
+    })
+  })
+})
+
+menusRouter.delete('/:menuId', (req, res, next) => {
+  const sql = 'SELECT * FROM MenuItem WHERE menu_id = $id'
+  const params = { $id: req.menu.id }
+  db.all(sql, params, (error, rows) => {
+    if (error) {
+      return next(error)
+    }
+    if (rows.length !== 0) {
+      return res.sendStatus(400)
+    }
+    const sql = 'DELETE FROM Menu WHERE id = $id'
+    db.run(sql, params, (error) => {
+      if (error) {
+        return next(error)
+      }
+      res.sendStatus(204)
+    })
+  })
+})
+
 module.exports = menusRouter
